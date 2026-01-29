296747a3f4260ca55d43a70e04cd195b561ca8fa
SFENCE <sfence.software@gmail.com>
2026-01-29 05:20:26 +0100

    Add abulity to disallow one_hp commnad registration.

--- commands.lua
+++ commands.lua
@@ -23,23 +23,25 @@ core.register_chatcommand(settings.respawn_command, {
 	end,
 })
 
--- Chat command to set HP to 1 if player has 0 HP
-core.register_chatcommand(settings.one_hp_command, {
-	params = "",
-	description = S("Set your HP to 1 if you have 0 HP"),
-	privs = {},
-	func = function(player_name, params)
-		local player = core.get_player_by_name(player_name)
-		if not player then
-			return false, S("Player not found")
-		end
-		
-		local hp = player:get_hp()
-		if hp == 0 then
-			player:set_hp(1)
-			return true, S("Your HP has been set to 1")
-		else
-			return false, S("You already have HP: @1", hp)
-		end
-	end,
-})
+if settings.one_hp_command ~= "" then
+	-- Chat command to set HP to 1 if player has 0 HP
+	core.register_chatcommand(settings.one_hp_command, {
+		params = "",
+		description = S("Set your HP to 1 if you have 0 HP"),
+		privs = {},
+		func = function(player_name, params)
+			local player = core.get_player_by_name(player_name)
+			if not player then
+				return false, S("Player not found")
+			end
+			
+			local hp = player:get_hp()
+			if hp == 0 then
+				player:set_hp(1)
+				return true, S("Your HP has been set to 1")
+			else
+				return false, S("You already have HP: @1", hp)
+			end
+		end,
+	})
+end
